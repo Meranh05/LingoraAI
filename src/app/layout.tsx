a06d7f1be46a0346a51756f5602499f6449b7199
Mera <nhain2508@gmail.com>
2026-06-13 16:35:12 +0700

    Suppress extension hydration attributes at root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,9 @@ export default async function RootLayout({
     <html
       lang={viewer?.locale ?? "vi"}
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full">
+      <body className="min-h-full" suppressHydrationWarning>
         <TooltipProvider>
           <LocaleProvider initialLocale={viewer?.locale ?? "vi"}>
             <ExperienceProvider>
